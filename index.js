40df9d22ebd827ba0614dc53d758f20870fc3653
dashrath-sharma <dashrath9958@gmail.com>
2024-05-11 14:49:24 +0530

    resolved the issue with env file

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ const express = require('express');
 const app = express();
 app.use(express.static('public'));
 
-const port = process.env.PORT | 8000;
+const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
     console.log(`Server running on PORT: ${port}`);
+    console.log(`Server PORT from ENV is ${process.env.PORT}`)
 })
